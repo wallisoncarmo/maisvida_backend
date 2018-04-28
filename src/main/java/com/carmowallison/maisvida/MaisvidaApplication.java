fcd7c93be4e6215ab9bf4d94ce22c51b75a8fc08
Wallison do Carmo Costa <wallisoncarmo01@gmail.com>
2018-04-28 10:39:13 -0300

    [ADD] Criacao do projeto

---
+++ src/main/java/com/carmowallison/maisvida/MaisvidaApplication.java
@@ -0,0 +1,12 @@
+package com.carmowallison.maisvida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaisvidaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaisvidaApplication.class, args);
+	}
+}
