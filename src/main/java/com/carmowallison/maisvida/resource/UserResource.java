feec6b13a18cf973f8cdf341d372c5c405cc932b
Wallison do Carmo Costa <wallisoncarmo01@gmail.com>
2018-04-28 15:15:10 -0300

    [ADD] Endpoint para atualizar o user

--- src/main/java/com/carmowallison/maisvida/resource/UserResource.java
+++ src/main/java/com/carmowallison/maisvida/resource/UserResource.java
@@ -46,6 +46,14 @@ public class UserResource {
 		return ResponseEntity.created(uri).build();
 	}
 	
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@PathVariable String id, @RequestBody UserNewDTO objDTO) {
+		User obj = service.fromDTO(objDTO);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+	
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable String id) {
 		service.delete(id);
--- src/main/java/com/carmowallison/maisvida/services/UserService.java
+++ src/main/java/com/carmowallison/maisvida/services/UserService.java
@@ -26,20 +26,50 @@ public class UserService {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-	
+
 	public User insert(User obj) {
 		return repository.insert(obj);
 	}
+
 	public void delete(String id) {
 		findById(id);
 		repository.deleteById(id);
 	}
-	
-	public User fromDTO(UserDTO objDTO) {
-		return new User(objDTO.getId(), objDTO.getFirst_name(), objDTO.getLastName(),objDTO.getEmail(), objDTO.isActive(), objDTO.isStatus(),null);
+
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+
+		return repository.save(newObj);
 	}
-	
+
+	private void updateData(User newObj, User obj) {
+
+		newObj.setActive(obj.isActive());
+		newObj.setStatus(obj.isStatus());
+
+		if (obj.getEmail() != null) {
+			newObj.setEmail(obj.getEmail());
+		}
+		if (obj.getFirstName() != null) {
+			newObj.setFirstName(obj.getFirstName());
+		}
+		if (obj.getLastName() != null) {
+			newObj.setLastName(obj.getLastName());
+		}
+		if (obj.getSenha() != null) {
+			newObj.setSenha(obj.getSenha());
+		}		
+		
+	}
+
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getFirst_name(), objDTO.getLastName(), objDTO.getEmail(),
+				objDTO.isActive(), objDTO.isStatus(), null);
+	}
+
 	public User fromDTO(UserNewDTO objDTO) {
-		return new User(null, objDTO.getFirst_name(), objDTO.getLastName(),objDTO.getEmail(), objDTO.isActive(), objDTO.isStatus(), objDTO.getSenha());
+		return new User(null, objDTO.getFirst_name(), objDTO.getLastName(), objDTO.getEmail(), objDTO.isActive(),
+				objDTO.isStatus(), objDTO.getSenha());
 	}
 }
